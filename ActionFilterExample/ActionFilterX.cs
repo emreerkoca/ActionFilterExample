57ac8d121379828b59d9ff3d2b5f04c9f5ce1e1d
Emre Erkoca <emreerkoca@outlook.com>
2018-06-05 00:24:12 +0300

    Action Filter Attribute Bacic Project
    First commit ;)

---
+++ ActionFilterExample/ActionFilterX.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterExample
+{
+    public class ActionFilterX : ActionFilterAttribute , IActionFilter
+    {
+        //Action tetiklenmeden once buraya gelir
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            StreamWriter strWriter = new StreamWriter(filterContext.HttpContext.Server.MapPath("~/log.txt"), true);
+            strWriter.WriteLine(filterContext.ToString() + " - " + DateTime.Now.ToString());
+            strWriter.Close();
+            base.OnActionExecuting(filterContext);
+        }
+
+        //Action' daki islemler bittikten sonra buraya gelir.
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            StreamWriter strWriter = new StreamWriter(filterContext.HttpContext.Server.MapPath("~/log.txt"),true);
+            strWriter.WriteLine(filterContext.ToString() + " - " +DateTime.Now.ToString());
+            strWriter.Close();
+            base.OnActionExecuted(filterContext);
+        }
+
+        //Action' daki islemler bittiginde buraya gelir. OnActionExecuted bu metoddan onceliklidir.
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            StreamWriter strWriter = new StreamWriter(filterContext.HttpContext.Server.MapPath("~/log.txt"), true);
+            strWriter.WriteLine(filterContext.ToString() + " - " + DateTime.Now.ToString());
+            strWriter.Close();
+            base.OnResultExecuting(filterContext);
+        }
+
+        //Action' daki islemler bittiğinde buraya gelir.  OnResultExecuting bu metoddan onceliklidir.
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            StreamWriter strWriter = new StreamWriter(filterContext.HttpContext.Server.MapPath("~/log.txt"), true);
+            strWriter.WriteLine(filterContext.ToString() + " - " + DateTime.Now.ToString());
+            strWriter.Close();
+            base.OnResultExecuted(filterContext);
+        }
+    }
+}
---
+++ ActionFilterExample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ActionFilterX]
+        public ActionResult Index()
+        {
+            StreamWriter strWriter = new StreamWriter(HttpContext.Server.MapPath("~/log.txt"), true);
+            strWriter.WriteLine(this.ControllerContext.RouteData.Values["action"].ToString() + " - " + DateTime.Now.ToString());
+            strWriter.Close();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
